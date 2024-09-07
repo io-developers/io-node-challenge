7935336601e2dad715e56ac675244b146cda0f74
rhuaman <rhuaman@dtransforma.com>
2024-09-07 16:33:08 -0500

    FEAUTURE: add config init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world");
+import * as express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
