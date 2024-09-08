ffd658910bee6051b469429050a10610799f44d3
rhuaman <rhuaman@dtransforma.com>
2024-09-07 19:54:15 -0500

    REFACTOR: se agrego la capa de dominio del bounded context transaction

---
+++ src/hexagonal/transaction/domain/Transaction.ts
@@ -0,0 +1,35 @@
+import { TransactionDataVo } from "./value-object/transaction-data.vo";
+import { TransactionIdVo } from "./value-object/transaction-id.vo";
+import { TransactionSourceVo } from "./value-object/transaction-source.vo";
+
+export class Transaction{
+    source: TransactionSourceVo;
+    id:TransactionIdVo;
+    data: TransactionDataVo;
+
+    constructor(soruce:TransactionSourceVo,id:TransactionIdVo,data:TransactionDataVo) {
+        this.source = soruce;
+        this.id = id;
+        this.data = data;
+    }
+
+    mapToPrimitives(){
+        return {
+            source: this.source.value,
+            id: this.id.value,
+            data:{
+                accountId: this.data.accountId,
+                amount: this.data.amount
+            } 
+        }
+    }
+}
+
+{
+    "source": "{uuid}",
+    "id": "{id}",
+    "data": {
+        "accountId": "{id}",
+        "amount": "{number}"
+    }
+}
